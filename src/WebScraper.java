fb34bb344494e1c5d182a806fc15eefd7fb0f60f
rachita <rnelat2@illinois.edu>
2018-02-07 18:36:08 -0600

    initial project commit.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -5,6 +5,7 @@ public class WebScraper {
 
     /**
      * Retrieve contents from a URL and return them as a string.
+     * 
      *
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
